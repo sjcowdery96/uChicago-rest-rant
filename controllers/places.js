185f5477d4c5a1107b9196a2728adf6ddce5c6a9
sjcowdery96 <sjc.cowdery@gmail.com>
2023-10-31 22:13:18 -0500

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,10 +1,24 @@
 const router = require('express').Router()
+const places = require("../models/places.js")
 
 //post method to create new
 router.post('/', (req, res) => {
     console.log(req.body)
-    res.send('POST /places')
+    if (!req.body.pic) {
+        // Default image if one is not provided
+        req.body.pic = 'http://placekitten.com/400/400'
+    }
+    //logic checks for missing data in the body
+    if (!req.body.city) {
+        req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+        req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
 })
+
 
 //need to fetch new places before we GET existing places
 router.get('/new', (req, res) => {
@@ -13,50 +27,6 @@ router.get('/new', (req, res) => {
 
 // GET /places
 router.get('/', (req, res) => {
-    let places = [
-        {
-            name: "Coding Cat Cafe",
-            city: "Phoenix",
-            state: "AZ",
-            cuisines: "Coffee, Bakery",
-            pic: "/images/r1.jpg"
-        },
-        {
-            name: "The Flying Pig",
-            city: "New York City",
-            state: "NY",
-            cuisines: "Mediterranean, Fusion",
-            pic: "/images/r2.jpg"
-        },
-        {
-            name: "The Dragon's Lair",
-            city: "Los Angeles",
-            state: "CA",
-            cuisines: "Chinese, Sichuan",
-            pic: "/images/r3.jpg"
-        },
-        {
-            name: "The Enchanted Forest",
-            city: "San Francisco",
-            state: "CA",
-            cuisines: "Vegetarian, Vegan",
-            pic: "/images/r4.jpg"
-        },
-        {
-            name: "The Windy City Diner",
-            city: "Chicago",
-            state: "IL",
-            cuisines: "American, Classic",
-            pic: "/images/r5.jpg"
-        },
-        {
-            name: "The Lone Star BBQ",
-            city: "Austin",
-            state: "TX",
-            cuisines: "BBQ, Tex-Mex",
-            pic: "/images/r6.jpg"
-        }
-    ]
     res.render('places/index', { places })
 })
 
---
+++ models/places.js
@@ -0,0 +1,44 @@
+module.exports = [
+    {
+        name: "Coding Cat Cafe",
+        city: "Phoenix",
+        state: "AZ",
+        cuisines: "Coffee, Bakery",
+        pic: "/images/r1.jpg"
+    },
+    {
+        name: "The Flying Pig",
+        city: "New York City",
+        state: "NY",
+        cuisines: "Mediterranean, Fusion",
+        pic: "/images/r2.jpg"
+    },
+    {
+        name: "The Dragon's Lair",
+        city: "Los Angeles",
+        state: "CA",
+        cuisines: "Chinese, Sichuan",
+        pic: "/images/r3.jpg"
+    },
+    {
+        name: "The Enchanted Forest",
+        city: "San Francisco",
+        state: "CA",
+        cuisines: "Vegetarian, Vegan",
+        pic: "/images/r4.jpg"
+    },
+    {
+        name: "The Windy City Diner",
+        city: "Chicago",
+        state: "IL",
+        cuisines: "American, Classic",
+        pic: "/images/r5.jpg"
+    },
+    {
+        name: "The Lone Star BBQ",
+        city: "Austin",
+        state: "TX",
+        cuisines: "BBQ, Tex-Mex",
+        pic: "/images/r6.jpg"
+    }
+]
